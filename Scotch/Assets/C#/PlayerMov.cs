5c1e42475f49864dff60941d83949cc6691a9795
GiuliaLippi <lippimarchettigiulia@gmail.com>
2024-10-02 18:29:24 +0200

    yeee

--- Scotch/Assets/C#/PlayerMov.cs
+++ Scotch/Assets/C#/PlayerMov.cs
@@ -6,23 +6,27 @@ public class PlayerMov : MonoBehaviour
 {
  public float horizontalInput;
     public float verticalInput;
+    
 
     public float speed;
-    public float turnSpeed = 10;
+    public float jumpStrenght = 10;
+    public Rigidbody rb;
 
     void Start()
     {
-
+     rb = GetComponent<Rigidbody>();
     }
     void Update()
     {
+        if(Mathf.Approximately(rb.velocity.y,0) && Input.GetButtonDown ("Jump"))
+          rb.velocity = new Vector3(rb.velocity.x,jumpStrenght,rb.velocity.z);
         // read values from keyboard
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
 
         // move the object 
         transform.Translate(Vector3.forward * Time.deltaTime * verticalInput);
-        //transform.Translate(-Vector3.right * Time.deltaTime * horizontalInput);
-        transform.Rotate(Vector3.up * horizontalInput * turnSpeed * Time.deltaTime );
+        transform.Translate(Vector3.right * Time.deltaTime * horizontalInput);
+        
     }
 }
