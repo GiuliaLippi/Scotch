ed4d8583354f09810c7ad1ec86412ffffbab1ab0
GiuliaLippi <lippimarchettigiulia@gmail.com>
2024-10-03 12:50:30 +0200

    Update
    Prefacb + MovPL C#

---
+++ Scotch/Assets/C#/PlayerMov2.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMov2 : MonoBehaviour
+{
+     public float speed = 6f;         // Velocità del Player
+    public float jumpForce = 5f;     // Forza del salto
+    public float gravityMultiplier = 2f; // Moltiplicatore della gravità
+
+    private Rigidbody rb;
+    private bool isGrounded;
+
+    // Controllo per sapere se è a terra
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    void Start()
+    {
+        // Prendiamo il Rigidbody dal Player
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true; // Evitiamo la rotazione del Player causata da collisioni
+    }
+
+    void Update()
+    {
+        // Controlliamo se il Player è a terra
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        // Movimento orizzontale
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        // Aggiungiamo il movimento alla velocità
+        rb.velocity = new Vector3(move.x * speed, rb.velocity.y, move.z * speed);
+
+        // Salto
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+
+        // Aggiunge gravità extra per rendere il salto più realistico
+        if (!isGrounded)
+        {
+            rb.AddForce(Vector3.down * gravityMultiplier, ForceMode.Acceleration);
+        }
+    }
+}
--- Scotch/Assets/C#/gestoreOstacoli.cs
+++ Scotch/Assets/C#/gestoreOstacoli.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class gestoreOstacoli : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+   IEnumerator InstantiateWithDelay(GameObject prefab, Vector3 position, float delay, int amount)
+{
+    for(int i = 1 ; i <= amount;i++)
     {
-        
-    }
+        yield return new WaitForSeconds(delay);
+        Object.Instantiate(prefab, position, Quaternion.identity);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
+}
+
+void Start()
+{
+    GameObject Mauro1 = Resources.Load("Mauro1") as GameObject;
+    GameObject Mauro2 = Resources.Load("Mauro2") as GameObject;
+    GameObject Mauro3 = Resources.Load("Mauro3") as GameObject;
+    StartCoroutine(InstantiateWithDelay(Mauro1, new Vector3(), 3f, 1));
+   
+
+
+}
+
+void Update(){
+
+}
 }
