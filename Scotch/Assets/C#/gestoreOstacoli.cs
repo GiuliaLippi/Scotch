e6119dd64ec8126ea5188e414ab4b7c18d4035e6
GiuliaLippi <lippimarchettigiulia@gmail.com>
2024-10-06 17:37:52 +0200

    LevTwo
    Livello 2

--- Scotch/Assets/C#/gestoreOstacoli.cs
+++ Scotch/Assets/C#/gestoreOstacoli.cs
@@ -44,6 +44,56 @@ void Start()
     StartCoroutine(InstantiateWithDelay(Mauro2, new Vector3(-12.72f, 0.97f, -5.48f), 20f, 1));
     StartCoroutine(InstantiateWithDelay(Mauro3, new Vector3(-12.72f, 0.97f, -7.86f), 21f, 1));
 
+    StartCoroutine(InstantiateWithDelay(Mauro4, new Vector3(14.65f, 0.97f, -2.96f), 25f, 1));
+    StartCoroutine(InstantiateWithDelay(Mauro3, new Vector3(-12.72f, 0.97f, -7.86f), 26f, 1));
+
+    StartCoroutine(InstantiateWithDelay(Mauro6, new Vector3(14.65f, 0.97f, -7.86f), 30f, 1));
+    StartCoroutine(InstantiateWithDelay(Mauro1, new Vector3(-12.72f, 0.97f, -3.6f), 31f, 1));
+    StartCoroutine(InstantiateWithDelay(Mauro5, new Vector3(14.65f, 0.97f, -5.48f), 32f, 1));
+
+    StartCoroutine(InstantiateWithDelay(Mauro1, new Vector3(-12.72f, 0.97f, -3.6f), 35f, 1));
+    StartCoroutine(InstantiateWithDelay(Mauro2, new Vector3(-12.72f, 0.97f, -5.48f), 35f, 1));
+    StartCoroutine(InstantiateWithDelay(Mauro3, new Vector3(-12.72f, 0.97f, -7.86f), 35f, 1));
+    StartCoroutine(InstantiateWithDelay(Mauro1, new Vector3(-12.72f, 0.97f, -3.6f), 37f, 1));
+    StartCoroutine(InstantiateWithDelay(Mauro2, new Vector3(-12.72f, 0.97f, -5.48f), 37f, 1));
+    StartCoroutine(InstantiateWithDelay(Mauro3, new Vector3(-12.72f, 0.97f, -7.86f), 37f, 1));
+
+    StartCoroutine(InstantiateWithDelay(Mauro1, new Vector3(-12.72f, 0.97f, -3.6f), 40f, 1));
+    StartCoroutine(InstantiateWithDelay(Mauro5, new Vector3(14.65f, 0.97f, -5.48f), 40f, 1));
+    StartCoroutine(InstantiateWithDelay(Mauro6, new Vector3(14.65f, 0.97f, -7.86f), 40f, 1));
+
+    StartCoroutine(InstantiateWithDelay(Mauro4, new Vector3(14.65f, 0.97f, -2.96f), 44f, 1));
+    StartCoroutine(InstantiateWithDelay(Mauro5, new Vector3(14.65f, 0.97f, -5.48f), 44f, 1));
+    StartCoroutine(InstantiateWithDelay(Mauro6, new Vector3(14.65f, 0.97f, -7.86f), 44f, 1));
+    StartCoroutine(InstantiateWithDelay(Mauro4, new Vector3(14.65f, 0.97f, -2.96f), 47f, 1));
+    StartCoroutine(InstantiateWithDelay(Mauro6, new Vector3(14.65f, 0.97f, -7.86f), 47f, 1));
+    StartCoroutine(InstantiateWithDelay(Mauro5, new Vector3(14.65f, 0.97f, -5.48f), 46f, 1));
+
+    StartCoroutine(InstantiateWithDelay(Mauro5, new Vector3(14.65f, 0.97f, -5.48f), 50f, 1));
+    StartCoroutine(InstantiateWithDelay(Mauro6, new Vector3(14.65f, 0.97f, -7.86f), 52f, 1));
+    StartCoroutine(InstantiateWithDelay(Mauro5, new Vector3(14.65f, 0.97f, -5.48f), 52f, 1));
+    StartCoroutine(InstantiateWithDelay(Mauro1, new Vector3(-12.72f, 0.97f, -3.6f), 52f, 1));
+    StartCoroutine(InstantiateWithDelay(Mauro5, new Vector3(14.65f, 0.97f, -5.48f), 54f, 1));
+
+    StartCoroutine(InstantiateWithDelay(Mauro1, new Vector3(-12.72f, 0.97f, -3.6f), 57f, 1));
+    StartCoroutine(InstantiateWithDelay(Mauro2, new Vector3(-12.72f, 0.97f, -5.48f), 57f, 1));
+    StartCoroutine(InstantiateWithDelay(Mauro3, new Vector3(-12.72f, 0.97f, -7.86f), 57f, 1));
+    StartCoroutine(InstantiateWithDelay(Mauro2, new Vector3(-12.72f, 0.97f, -5.48f), 58f, 1));
+    StartCoroutine(InstantiateWithDelay(Mauro5, new Vector3(14.65f, 0.97f, -5.48f), 60f, 1));
+    StartCoroutine(InstantiateWithDelay(Mauro6, new Vector3(14.65f, 0.97f, -7.86f), 60f, 1));
+
+    StartCoroutine(InstantiateWithDelay(Mauro4, new Vector3(14.65f, 0.97f, -2.96f), 64f, 1));
+    StartCoroutine(InstantiateWithDelay(Mauro5, new Vector3(14.65f, 0.97f, -5.48f), 64f, 1));
+    StartCoroutine(InstantiateWithDelay(Mauro6, new Vector3(14.65f, 0.97f, -7.86f), 64f, 1));
+    StartCoroutine(InstantiateWithDelay(Mauro4, new Vector3(14.65f, 0.97f, -2.96f), 65f, 1));
+    StartCoroutine(InstantiateWithDelay(Mauro5, new Vector3(14.65f, 0.97f, -5.48f), 65f, 1));
+    StartCoroutine(InstantiateWithDelay(Mauro6, new Vector3(14.65f, 0.97f, -7.86f), 65f, 1));
+
+
+
+
+
+
 
 
 
