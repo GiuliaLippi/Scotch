b16c819f82e00f88eb7bf1f4aa4c279adfccc467
GiuliaLippi <lippimarchettigiulia@gmail.com>
2024-10-07 17:14:47 +0200

    Update
    Messo in scena Level3

--- Scotch/Assets/C#/Gestoreostacoli_livelloTre.cs
+++ Scotch/Assets/C#/Gestoreostacoli_livelloTre.cs
@@ -22,10 +22,41 @@ void Start()
     GameObject SferaLenta4 = Resources.Load("Sfera4_Lenta") as GameObject;
   
 
+
     StartCoroutine(InstantiateWithDelay(SferaLenta1, new Vector3(-4.51f, 2.81f, -0.51f), 2f, 1));
-    StartCoroutine(InstantiateWithDelay(SferaLenta2, new Vector3(-0.94f, 2.81f, -0.65f), 2f, 1));
-    StartCoroutine(InstantiateWithDelay(SferaLenta3, new Vector3(3.17f, 2.81f, -0.68f), 2f, 1));
-    StartCoroutine(InstantiateWithDelay(SferaLenta4, new Vector3(6.72f, 2.81f, -0.6f), 2f, 1));
+
+    StartCoroutine(InstantiateWithDelay(SferaLenta3, new Vector3(3.17f, 2.81f, -0.68f), 5f, 1));
+
+    StartCoroutine(InstantiateWithDelay(SferaLenta4, new Vector3(6.72f, 2.81f, -0.6f), 8f, 1));
+    StartCoroutine(InstantiateWithDelay(SferaLenta2, new Vector3(-0.94f, 2.81f, -0.65f), 8f, 1));
+
+    StartCoroutine(InstantiateWithDelay(SferaLenta2, new Vector3(-0.94f, 2.81f, -0.65f), 11f, 1));
+    StartCoroutine(InstantiateWithDelay(SferaLenta1, new Vector3(-4.51f, 2.81f, -0.51f), 11f, 1));
+
+    StartCoroutine(InstantiateWithDelay(SferaLenta4, new Vector3(6.72f, 2.81f, -0.6f), 14f, 1));
+
+    StartCoroutine(InstantiateWithDelay(SferaLenta1, new Vector3(-4.51f, 2.81f, -0.51f), 17f, 1));
+    StartCoroutine(InstantiateWithDelay(SferaLenta3, new Vector3(3.17f, 2.81f, -0.68f), 17f, 1));
+
+    StartCoroutine(InstantiateWithDelay(SferaLenta2, new Vector3(-0.94f, 2.81f, -0.65f), 18f, 1));
+
+    StartCoroutine(InstantiateWithDelay(SferaLenta3, new Vector3(3.17f, 2.81f, -0.68f), 20f, 1));
+    StartCoroutine(InstantiateWithDelay(SferaLenta4, new Vector3(6.72f, 2.81f, -0.6f), 19f, 1));
+
+    StartCoroutine(InstantiateWithDelay(SferaLenta1, new Vector3(-4.51f, 2.81f, -0.51f), 22f, 1));
+    StartCoroutine(InstantiateWithDelay(SferaLenta2, new Vector3(-0.94f, 2.81f, -0.65f), 23f, 1));
+    StartCoroutine(InstantiateWithDelay(SferaLenta3, new Vector3(3.17f, 2.81f, -0.68f), 24f, 1));
+
+    StartCoroutine(InstantiateWithDelay(SferaLenta4, new Vector3(6.72f, 2.81f, -0.6f), 26f, 1));
+    StartCoroutine(InstantiateWithDelay(SferaLenta1, new Vector3(-4.51f, 2.81f, -0.51f), 26f, 1));
+
+    StartCoroutine(InstantiateWithDelay(SferaLenta2, new Vector3(-0.94f, 2.81f, -0.65f), 28f, 1));
+    StartCoroutine(InstantiateWithDelay(SferaLenta3, new Vector3(3.17f, 2.81f, -0.68f), 28f, 1));
+
+
+
+
+    
 
     
 
